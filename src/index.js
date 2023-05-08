56109880c130652fe1470a025cb6fc72d7ce58e2
rdev32 <rdevelops32@gmail.com>
2023-05-08 16:30:27 -0500

    feat: add notion api request

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import 'dotenv/config'
 import { Client, GatewayIntentBits } from 'discord.js'
-import axios from 'axios'
 
 const client = new Client({
     intents: [
@@ -16,10 +15,46 @@ client.on('interactionCreate', async interaction => {
     if (interaction.commandName !== 'idea') return
 
     try {
-        await axios.post(process.env.URL, 
-            JSON.stringify({ 'title': interaction.options.getString('content') })
-        )
-    } catch (error) {
+        await fetch(process.env.URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Notion-Version': '2022-06-28',
+                'Authorization': `Bearer ${process.env.NOTION}`
+            },
+            body: JSON.stringify({
+                'parent': {
+                    'database_id': process.env.db_id
+                },
+                'properties': {
+                    'Name': {
+                        'title': [{
+                            'text': {
+                                'content': `Idea from ${interaction.user.id}`// row name
+                            }
+                        }]
+                    },
+                    'Description': {
+                        'rich_text': [{
+                            'text': {
+                                'content': interaction.options.getString('content') // row description
+                            }
+                        }]
+                    },
+                    'Tags': {
+                        'multi_select' : [
+                            {
+                                'name': 'tag 1'
+                            },
+                            {
+                                'name': 'tag 2'
+                            }
+                        ]
+                    }
+                }
+            })
+        })
+        } catch (error) {
         console.error(error.message)
     }
 
